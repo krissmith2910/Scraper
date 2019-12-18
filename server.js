c3bdff66bcd0b5fa57bf7fcf98a206f138174042
Kristopher Smith <ksmith2910@outlook.com>
2019-12-17 20:42:35 -0700

    updated all files

--- server.js
+++ server.js
@@ -1,52 +1,36 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
-
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
+
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
-// Routes
 
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
+  
   axios.get("http://www.bioethics.net/news").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
     $("h5").each(function(i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children("a")
         .text();
@@ -54,40 +38,29 @@ app.get("/scrape", function(req, res) {
         .children("a")
         .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
     });
 
-    // Send a message to the client
     res.send("Scrape Complete!");
   });
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
-    // Find all results from the scrapedData collection in the db
     db.Article.find()
-      // Throw any errors to the console
       .then(function(dbPopulate) {
-        // If any Libraries are found, send them to the client with any associated Books
         res.json(dbPopulate);
       })
       .catch(function(err) {
-        // If an error occurs, send it back to the client
         res.json(err);
       });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
   // TODO
   // ====
@@ -97,11 +70,9 @@ app.get("/articles/:id", function(req, res) {
   db.Article.findById(req.params.id)
   .populate("note")
   .then(function(dbPopulate) {
-    // If any Libraries are found, send them to the client with any associated Books
     res.json(dbPopulate);
   })
   .catch(function(err) {
-    // If an error occurs, send it back to the client
     res.json(err);
   });
 });
@@ -119,11 +90,9 @@ app.post("/articles/:id", function(req, res) {
       return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbPopulate._id } }, { new: true });
     })
     .then(function(dbPopulate) {
-      // If the Library was updated successfully, send it back to the client
       res.json(dbPopulate);
     })
     .catch(function(err) {
-      // If an error occurs, send it back to the client
       res.json(err);
     });
 });
